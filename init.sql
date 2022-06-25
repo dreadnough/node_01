37e179a4403663f0581a9e86438815d93f00265a
Alex Melnyk <mel.poi97@gmail.com>
2022-06-25 17:44:22 +0300

    Add HomeWork Alex Melnyk. Update init.sql

--- init.sql
+++ init.sql
@@ -44,3 +44,56 @@ VALUES ("Joker", "psychological thriller", "Todd Phillips", 2019),
 ("Jurassic World", "action", "Colin Trevorrow", 2015);
 
 
+
+--START HomeWork Alex Melnyk
+
+-- CREATE DB
+CREATE DATABASE database_demo;
+USE database_demo;
+
+-- CREATE TABLE
+
+CREATE TABLE user_info (
+	name_user VARCHAR(100) NOT NULL,
+	age_user INT(11) NOT NULL,
+	country_user TEXT NOT NULL  
+);
+
+CREATE TABLE problems_info (
+	names_problems VARCHAR(100) NOT NULL,
+	deskription_problems TEXT NOT NULL
+);
+
+CREATE TABLE programming_date (
+	name_language VARCHAR(100) NULL DEFAULT '',
+	years_experience SMALLINT(11) NULL DEFAULT NULL,
+	year_start TINYINT(11) NULL DEFAULT NULL
+);
+
+
+-- INSERT
+
+INSERT INTO user_info (name_user, age_user, country_user) 
+VALUES ("Alex", 25, "Ukraine"),
+("Ivan", 21, "Ukraine"),
+("Inna", 31, "Ukraine"),
+("Vlad", 27, "Ukraine"),
+("Illia", 23, "Ukraine");
+
+INSERT INTO programming_date (name_language, years_experience, year_start) 
+VALUES ("JavaScript", 2, 20),
+("Python", 1, 22),
+("C#",5, 17),
+("Rust", 1, 21),
+("C++", 12, 10);
+
+INSERT INTO problems_info (names_problems, deskription_problems) 
+VALUES ("screen breakage", "the screen is cracked and does not work"),
+("touchpad", "not work"),
+("screen breakage", "the screen is cracked and does not work"),
+("touchpad", "not work"),
+("screen breakage", "the screen is cracked and does not work");
+
+-- End HomeWork Alex Melnyk
+
+
