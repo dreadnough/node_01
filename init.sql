97fb1357e7347257068471f1a365bd8a0c1de491
Polina N <polinanizhynska@gmail.com>
2022-06-26 21:09:57 +0400

    Add fixed SQL Nizhynska

--- init.sql
+++ init.sql
@@ -93,40 +93,40 @@ VALUES ("screen breakage", "the screen is cracked and does not work"),
 -- End HomeWork Alex Melnyk
 
 -- Start homework Polina Nizhynska
-CREATE TABLE Streets (
-    Coutry varchar(30),
-    City varchar(30),
-    StreetName varchar(30)
+CREATE TABLE streets (
+    coutry varchar(30),
+    city varchar(30),
+    street_name varchar(30)
 );
-CREATE TABLE Taxes (
-    YourName varchar(30),
-    Surname varchar(30),
-    Resedents varchar(30),
-    Ocupation varchar(30),
-	Income int
+CREATE TABLE taxes (
+    your_name varchar(30),
+    surname varchar(30),
+    resedents varchar(30),
+    ocupation varchar(30),
+	income int
 );
-CREATE TABLE BookTikets (
-    YourName varchar(30),
-    Surname varchar(30),
-    Departure varchar(30),
-    Destination varchar(30)
+CREATE TABLE book_tikets (
+    your_name varchar(30),
+    surname varchar(30),
+    departure varchar(30),
+    destination varchar(30)
 );
 
-insert into BookTikets values 
+insert into book_tikets values 
 ("Volodymyr", "Ivanov", "Lviv", "Ternopil"),
 ("Vitaliy", "Selyshyn", "Gdansk", "Poznan"),
 ("Olexandr", "Semenyk", "Berlin", "Katovice"), 
 ("Sergiy", "Melnyk", "Chernivtsi", "Lviv"),
 ("Anatoliy", "Mosiychyk", "Ivano-Frankivsk", "Peremyshl");
 
-insert into Streets values 
+insert into streets values 
 ("Ukraine", "Lviv", "Kopernyka"),
 ("Ukraine", "Uzgorod", "Shevchenka"),
 ("Poland", "Warsaw", "Żelazna"), 
 ("Germany", "Berlin", "Schröderstraße"),
 ("France", "Paris", "Rue de Rivoli"); 
 
-insert into Taxes values 
+insert into taxes values 
 ("Ivan", "Zadoroznyy", "Accounter", "Ukraine", 50000),
 ("Max", "Stepanovych", "Developer", "Ukraine", 40000),
 ("Volodymyr", "Kuziz", "Analyst","Ukraine", 100000), 
