d579bf0bc7e8f2f86087e2c1499a03e4f681fc71
Sofiia Madryha <sonyamadryha3@gmail.com>
2022-06-28 15:43:44 +0300

    add types of join

--- select.sql
+++ select.sql
@@ -37,3 +37,25 @@ CROSS JOIN projects
 ON films.idfm=projects.project_id;
 
 -- Alex Melnyk homework finish
+
+-- Sofiia Madryha homework start
+SELECT movies.movies_id, movies.title, viewers.vw_name
+FROM movies 
+INNER JOIN viewers 
+ON movies.movies_id = viewers.idvw;
+  
+SELECT  p.pr_name, p.pr_price, b.title
+FROM products AS p
+LEFT JOIN books AS b
+ON b.price = p.pr_price;
+
+SELECT d.dir_name, d.id_suname, d.dir_age, a.age
+FROM actors AS a
+RIGHT JOIN directors AS d
+ON a.age = d.dir_age;
+
+SELECT b.title, b.price, c.cust_id
+FROM books AS b 
+CROSS JOIN customers AS c
+ON b.user_id = c.cust_id;
+-- Sofiia Madryha homework finish
