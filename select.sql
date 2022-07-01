3a1ed8771aea77ef469019d305867ab178482cf8
Gerburt <gerburt967@gmail.com>
2022-07-01 15:00:16 +0300

    Homework

--- select.sql
+++ select.sql
@@ -16,4 +16,19 @@ RIGHT JOIN user
 ON user.id=books.user_id
 WHERE books.id=2;
 
+SELECT * FROM user 
+GROUP BY name 
+HAVING name="Andriy";
+
+SELECT age 
+FROM user 
+GROUP BY age 
+HAVING age>=29;
+
+SELECT price, SUM(price) 
+AS sum
+FROM books
+GROUP BY price
+HAVING sum>=300;
+
 -- Andriy Voitiv homework finish
