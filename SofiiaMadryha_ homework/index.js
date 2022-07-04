3aa905c03669fa6abef775515fcb096f8e690eef
Sofiia Madryha <sonyamadryha3@gmail.com>
2022-07-04 10:11:11 +0300

    js - homework

---
+++ SofiiaMadryha_ homework/index.js
@@ -0,0 +1,21 @@
+function sum (a,b){
+  return  a + b
+}
+
+function  sub (c,d){
+    return c - d
+}
+
+function mult (a,b){
+    return a * b
+}
+
+function div (c,d){
+    return c / d
+}
+
+
+console.log(sum(2,3))
+console.log(sub(6,2))
+console.log(mult(4,5))
+console.log(div(9,3))
