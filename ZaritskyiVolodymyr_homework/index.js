7092d41b182e892d161c05e46b4418bdcba1b726
Bondageman <97292216+Bondageman@users.noreply.github.com>
2022-07-04 15:53:17 +0300

    zaritskyi homework sql + js

---
+++ ZaritskyiVolodymyr_homework/index.js
@@ -0,0 +1,20 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const sub = (a, b) => {
+    return a - b;
+};
+
+const mul = (a, b) => {
+    return a * b;
+};
+
+const div = (a, b) => {
+    return a / b;
+};
+
+console.log(add(10, 5));
+console.log(sub(10, 5));
+console.log(mul(10, 5));
+console.log(div(10, 5));
