172bcec1db2dea33b4c5edf098a8b6d5c10dd3aa
Polina N <polinanizhynska@gmail.com>
2022-07-04 17:48:40 +0400

    Homework Polina Nizhynska JS

---
+++ PolinaNizhynska_homework/index.js
@@ -0,0 +1,45 @@
+function sum(a, b) {
+    let c;
+    c = a + b ;
+
+  return c;
+};
+
+
+function minus(a, b) {
+    let c;
+    c = a - b ;
+
+  return c;
+};
+
+
+function multiply(a, b) {
+    let c;
+    c = a * b ;
+
+  return c;
+};
+
+
+function multiply(a, b) {
+    let c;
+    c = a * b ;
+
+  return c;
+};
+
+multiply(3, 6);
+
+function devide(a, b) {
+    let c;
+    c = a / b ;
+
+  return c;
+};
+
+sum(4, 4);
+multiply(3, 6);
+minus(9, 4);
+devide(21, 7);
+
