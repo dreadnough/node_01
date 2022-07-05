5ac02ecbfd2c6813e7310de21415642c71e8f756
Halyna.Horbiak <halynalasia@gmail.com>
2022-07-06 02:56:44 +0300

    Comments by lilu

--- init.sql
+++ init.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE market;
 USE market;
+
+CREATE TABLE comments(
+    comment_id INT AUTO_INCREMENT, 
+    product_id VARCHAR(100) NOT NULL,
+    comment_text DATE, -- текст слово склюелю тому поставила комент текст!!!!
+    user_id DATE,
+    PRIMARY KEY(comment_id)
+);
