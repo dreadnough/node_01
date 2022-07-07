0961d5ac31a77c4a69f62689a56fc97cd17cfa87
Halyna.Horbiak <halynalasia@gmail.com>
2022-07-07 14:27:33 +0300

    js

---
+++ HalynaHorbiak_homework/index.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5,4));
+
+function minus(a, b) {
+    return a - b;
+  }
+  console.log(minus(5,4));
+
+function mnozenia(a, b) {
+    return a * b;
+  }
+  console.log(mnozenia(5,4));
+
+function dilenia(a, b) {
+    return a + b;
+  }
+  console.log(dilenia(5,4));
