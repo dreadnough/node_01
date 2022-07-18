afe737499e868345b171cd011e8b557634b3b47c
Ivan <odin_ogame@ukr.net>
2022-07-18 09:54:40 +0300

    style fix

--- market/server.js
+++ market/server.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 
 const routes = require("./routes");
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.use(routes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
