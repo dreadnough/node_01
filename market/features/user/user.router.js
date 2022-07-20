7971a89927f1e152098354497a5f4aca84467dd8
Ivan <odin_ogame@ukr.net>
2022-07-20 22:24:41 +0300

    add validator example

--- market/features/user/user.router.js
+++ market/features/user/user.router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const userController = require("./user.controller");
+const { validator } = require("../validator");
+const userValidator = require("./user.validator");
 
 app.get(
   `/`,
+  validator.body(userValidator.createUser.body),
   userController.getUsers
 );
 
