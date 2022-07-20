3d757b9c7a20fbc7dc84684ade2b6ec43d3a64aa
Alex Melnyk <mel.poi97@gmail.com>
2022-07-20 21:20:31 +0300

    Update user(added functionality for creating a new user)

--- market/features/user/user.model.js
+++ market/features/user/user.model.js
@@ -1,18 +1,32 @@
 const pool = require("../../mysql/mysqlPools").depotPool;
-const {
-    getResultOrEmptyArray
-} = require("../utils/mysql-helper");
+const { getResultOrEmptyArray } = require("../utils/mysql-helper");
 
-const getUsers = (conn = pool) => () => {
-  return conn
-    .query(
-      `
+const getUsers =
+    (conn = pool) =>
+    () => {
+        return conn
+            .query(
+                `
       SELECT * FROM users
-    `,
-    )
-    .then(getResultOrEmptyArray);
-};
+    `
+            )
+            .then(getResultOrEmptyArray);
+    };
+
+const createUsers =
+    (conn = pool) =>
+    ({ first_name, last_name, user_phone, user_city }) => {
+        return conn
+            .query(
+                `INSERT INTO users 
+            (first_name, last_name, user_phone, user_city)
+            VALUES(?,?,?,?);`,
+                [first_name, last_name, user_phone, user_city]
+            )
+            .then(getResultOrEmptyArray);
+    };
 
 module.exports = {
-    getUsers
+    getUsers,
+    createUsers,
 };
--- market/features/user/user.router.js
+++ market/features/user/user.router.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const userController = require("./user.controller");
 
-app.get(
-  `/`,
-  userController.getUsers
-);
+app.get(`/`, userController.getUsers);
+
+app.post(`/`, userController.createUsers);
 
 module.exports = app;
