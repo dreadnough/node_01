37a6fedd986ef6d4c4a6e0c2b18a44de693d508c
Sofiia Madryha <sonyamadryha3@gmail.com>
2022-08-03 20:50:11 +0300

    add functionality for create builging material product  # This is a combination of 4 commits.
    add functionality for get products
    
    add functionality for creat product build material
    
    change names of id in tables and correct mistakes
    
    add functionality for get products
    
    add functionality for creat product build material
    
    add validation for create building material product
    add functionality for create builging material product
    
    fix conflicts

---
+++ market/features/product/product.service.js
@@ -0,0 +1,17 @@
+const {getProducts, createProduct, createProductBuildMaterial} = require("./product.model");
+
+
+const getProductService = () => {
+    return getProducts()();
+  };
+
+const createProductBuildMaterialService = async (product) => {
+  const {insertId} = await createProduct()(product);
+  await createProductBuildMaterial()(insertId, product);
+  return insertId;
+  };
+
+ module.exports={
+    getProductService,
+    createProductBuildMaterialService
+};
